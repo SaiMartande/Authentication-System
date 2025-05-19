caa8fb9d4cfa25acd9715ec20a6a41231bfe2177
Sai Martande <saimartande@gmail.com>
2025-05-19 15:48:26 +0530

    Add backend and frontend folder to repo

---
+++ authentify-sb/src/test/java/com/authentify/AuthentifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
